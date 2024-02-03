8e3fb5905f6ae72aade042a2638f167b3d670950
TrippleAWap <3421carter24@gmail.com>
2024-02-02 17:01:04 -0700

    Type generation based off of protocol versions.

--- index.js
+++ index.js
@@ -1,36 +1,112 @@
-const { connectTypeCaching, getCaches } = require("./handleTypeCaching");
-const { Relay, createClient, Client } = require("bedrock-protocol");
-const relay = new Relay({
-	host: "0.0.0.0",
-	port: 19131,
-	/* Where to send upstream packets to */
-	destination: {
-		host: "ca.hivebedrock.network",
-		port: 19132,
-	},
-});
-relay.listen(); // Tell the server to start listening.
-
-const client = createClient({
-	host: "ca.hivebedrock.network",
-	port: 19132,
-	username: "test",
-});
-relay.on("connect", (player) => {
-	/** @type {Client} */
-	const client = player;
-	console.log("New connection", player.connection.address);
-	connectTypeCaching(player);
-	client.on("spawn", () => {
-		client.write("play_sound", {
-			name: "random.levelup",
-			position: client.position,
-			volume: 32767,
-			pitch: -32767,
-		});
+const fs = require("fs")
+const path = "./node_modules/minecraft-data/minecraft-data/data/bedrock/" + fs.readdirSync("./node_modules/minecraft-data/minecraft-data/data/bedrock/").filter(i => !isNaN(i.replace(/\./g, ""))).reverse()[0] + "/protocol.json";
+const currentTypes = require(path)
+const predefs = `
+// 8bit integer
+type i8 = "native"
+// unsigned 8bit integer
+type u8 = "native"
+// 16bit long int
+type li16 = "native"
+// 16bit long unsigned int
+type lu16 = "native"
+// 32bit int
+type i32 = "native"
+// 32bit long int
+type li32 = "native"
+// 32bit long unsigned int
+type lu32 = "native"
+// 32bit float
+type lf32 = "native"
+// 64bit long unsigned int
+type lu64 = "native"
+// 64bit long int
+type li64 = "native"
+type bool = boolean
+type varint = "native"
+`;
+function generateTypeScriptTypes(jsonData) {
+	const types = jsonData.types;
+	let output = "";
+	const entries = Object.entries(types);
+	const typeDefs = entries.filter(([_, i]) => typeof i === "string");
+	output += typeDefs
+		.map(([name, value]) => `type ${name} = "${value}"`)
+		.join("\n");
+	let interfaces = entries.filter(([_, data]) => typeof data !== "string");
+	// map interfaces
+	/**
+	 * @param {any[]} data
+	 */
+	const generateInterfaceData = (data) => {
+		let output = "";
+		// typeof data is always an array ( data_type\nObject ) repeats..
+		for (let i = 0; i < data.length / 2; i++) {
+			const type = data[i];
+			let typeData = data[i + 1];
+			switch (type) {
+				case "container": {
+					typeData = `{\n${typeData
+						.map(
+							(data) =>
+								`${data.name}: ${
+									typeof data.type === "object"
+										? generateInterfaceData(data.type)
+										: data.type
+								}`,
+						)
+						.join("\n")}\n}`;
+					break;
+				}
+				case "array": {
+					typeData = `{${typeData.countType}:${
+						typeof typeData.type === "object"
+							? generateInterfaceData(typeData.type)
+							: typeData.type
+					}[]}`;
+					break;
+				}
+				case "bitfield": {
+					let output = []
+					for (const entry of typeData) {
+						const line = `${entry.name}: {\n${Object.entries(entry).filter(([, value]) => value !== entry.name).map(([key, value]) => `${key}:${value}`).join("\n")}\n}`
+						output.push(line);
+					}
+					typeData = `{\n${output.join(",")}\n}`
+					break;
+				}
+				case "mapper": {
+					typeData = `{\n  type: ${typeData.type},\n  mappings: {\n${Object.entries(typeData.mappings)
+						.map(([key, value]) => `    "${key}": "${value}"`)
+						.join(",\n")}\n  }\n}`;
+					break;
+				}
+				case "switch": {
+					if (typeData.compareTo !== "name") break;
+					const output = []
+					for (const [key, value] of Object.entries(typeData.fields)) {
+						output.push(`${key}:${value.replace(/\s+![a-zA-Z0-9]/g, "")}`)
+					}
+					typeData = `{\n${output.join(",\n")}\n}`
+					break
+				}
+			}
+			output +=
+				typeof typeData === "object"
+					? JSON.stringify(typeData, undefined, 4)
+					: typeData;
+		}
+		return output;
+	};
+	interfaces = interfaces.map(([name, data]) => {
+		return [name, generateInterfaceData(data)];
 	});
-});
-setInterval(() => {
-	const { clientbound, serverbound } = getCaches();
-	console.log("\x1b[31m", "Clientbound:", "\x1b[0m", Object.keys(clientbound).length, "\x1b[31m", "Serverbound:", "\x1b[0m", Object.keys(serverbound).length);
-});
+	output += `\n${interfaces.filter(([name]) => name !== "string")
+		.map(([name, data]) => `export interface ${name} ${data}`)
+		.join("\n")}`;
+	return output;
+}
+
+const output = generateTypeScriptTypes(currentTypes);
+fs.writeFileSync("./output.ts", `${predefs}\n${output}`, { encoding: "utf-8" });
+console.log(`\x1b[36mCreated ${output.split("\n").length} lines! ( MCBE Protocol Version ${path.split("/").reverse()[1]} )\x1b[0m`)
--- utils.d.ts
+++ utils.d.ts
@@ -1,6 +1,13 @@
-import { CLIENTBOUND } from "TYPE_FILE_PATH";
+import { mcpe_packet } from "./output";
 declare module "bedrock-protocol" {
 	interface Client {
-		write: <T extends keyof CLIENTBOUND>(packetName: T, packetParams: CLIENTBOUND[T]) => void;
+		write: <T extends keyof mcpe_packet["params"]>(
+			packetName: T,
+			packetParams: mcpe_packet["params"][T]
+		) => void;
+		on: <T extends keyof mcpe_packet["params"]>(
+			packetName: T,
+			callback: (packetParams: mcpe_packet["params"][T]) => any
+		) => any;
 	}
 }
